069ba172f5c78bb914e7b50d1ee3fcee44d2b5ab
Basheer2001 <basheerayalkhiat@gmail.com>
2023-05-27 18:35:11 +0300

    ddf

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:dupro/ExpertProfile.dart';
 import 'package:dupro/UorE.dart';
 import 'package:dupro/local/local.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'UorE.dart';
 import 'local/localControler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'experts.dart';
 String Token='';
 void main()  {
   WidgetsFlutterBinding.ensureInitialized( );
